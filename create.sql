cf35df534815b6da296a6625a1aa7357180169db
Olle Bergkvist <ollebe@student.chalmers.se>
2013-08-31 18:22:49 +0200

    * Tabell för insättningar från webbtjänst

--- create.sql
+++ create.sql
@@ -19,6 +19,18 @@ CREATE TABLE rustvaluta.transactions (
 	timestamp timestamp NOT NULL
 );
 
-CREATE INDEX by_account ON rustvaluta.transactions ( account );
-CREATE INDEX by_timestamp ON rustvaluta.transactions ( timestamp );
+CREATE INDEX tra_by_account ON rustvaluta.transactions ( account );
+CREATE INDEX tra_by_timestamp ON rustvaluta.transactions ( timestamp );
+
+CREATE TABLE rustvaluta.deposits (
+	id SERIAL PRIMARY KEY NOT NULL,
+	account INT NOT NULL,
+	change FLOAT NOT NULL,
+	ip_address VARCHAR(100),
+	timestamp timestamp NOT NULL
+);
+
+CREATE INDEX dep_by_account ON rustvaluta.deposits ( account );
+CREATE INDEX dep_by_ip_address ON rustvaluta.deposits ( ip_address );
+CREATE INDEX dep_by_timestamp ON rustvaluta.deposits ( timestamp );
 
--- drop.sql
+++ drop.sql
@@ -1,6 +1,7 @@
 \connect mydb
 
 DROP TABLE rustvaluta.transactions ;
+DROP TABLE rustvaluta.deposits ;
 DROP TABLE rustvaluta.accounts ;
 DROP SCHEMA rustvaluta ;
 

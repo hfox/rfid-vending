9272c073956a189ad4da8f221e46fca74bd872a0
hfox <zehfox@gmail.com>
2013-09-06 00:38:26 +0200

    Cleaning proof-of-concept branch.

--- osproject/display.h
+++
@@ -1,9 +0,0 @@
-#define mainDISPLAY_PERIOD		( ( portTickType ) 3000 / portTICK_RATE_MS  )
-
-void vDisplay(void *pvParameters );
-
-void display_set_error(void);
-void display_set_has_card(void);
-void display_set_accept(void);
-void display_set_deny(void);
-
